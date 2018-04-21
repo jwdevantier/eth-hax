98c4281bf826827f1908438a551787b65e139de1
Ditlev Tøjner <d@asosio.com>
2018-04-21 19:41:16 +0200

    fixes

---
+++ scripts.js
@@ -0,0 +1,67 @@
+$( document ).ready(function() {
+
+    $("#nav li").on('click', function() {
+        var content = document.getElementById(this.id + "-content")
+        $(".container").css('display', 'none');
+        $(content).css('display', 'initial');
+    })
+
+    var Request = function(method, data, cb) {
+        
+        const reqConf = {
+            url: "http://localhost:8080",
+            type: method || "GET",
+            crossDomain: true,
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            success: function (response) {
+                var resp = JSON.parse(response)
+                cb(resp)
+            },
+            error: function (xhr, status) {
+                console.error("error", status, JSON.stringify(xhr, null, 2));
+            }
+        }
+
+        if (method !== "GET") {
+            reqConf['data'] = JSON.stringify(data)
+        }
+
+        $.ajax(reqConf);
+    }
+
+    var deposit = function(values) {
+        Request("POST", values, function(rsp) {
+            console.log(rsp)
+        })
+    }
+    
+    $(".form").submit(function(e) {
+        e.preventDefault()
+
+        var typeOfForm = $(this).attr('id')
+        var values = {typeOfForm}
+        $.each($(this).serializeArray(), function(i, field) {
+            values[field.name] = field.value;
+        });
+
+        switch (typeOfForm) {
+            case "deposit": return deposit(values)
+            case "withdraw": return withdraw(values)
+            case "check": return check(values)
+            default: return false
+        }
+    })
+
+    // $("form").on('submit', function(e) {
+    //     e.preventDefault()
+    //     var data = {}
+    //     $(this).each(function() {
+    //         var input = $(this).find(":input")
+    //         console.log(input)
+    //         data[input.attr("name")] = input.val()
+    //     })
+    //     console.log(data)
+    // })
+
+});
